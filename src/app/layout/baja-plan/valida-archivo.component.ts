903df1542291ab1ae6f55d81bafbd0aa54ba394c
omar_barra <omar_barra_v@hotmail.com>
2018-02-12 17:16:19 -0600

    prototipo valida archivo

--- src/app/layout/baja-plan/valida-archivo.component.ts
+++ src/app/layout/baja-plan/valida-archivo.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
-  selector: 'wsf-valida-archivo',
   templateUrl: './valida-archivo.component.html',
-  styleUrls: ['./valida-archivo.component.scss']
+  styleUrls: ['./valida-archivo.component.scss'],
+  animations: [routerTransition()]
 })
 export class ValidaArchivoComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    this.fixedHeader('idTablaInvalid');
+    this.fixedHeader('idTablaValid');
+  }
+
+  // Usa CSS transforms para dejar los titulos fijos en la tabla
+  fixedHeader(idTabla): void {
+    // Esperar a que se construya la tabla, delay de 1 segundo
+    setTimeout(function () {
+      document.getElementById(idTabla).addEventListener('scroll', function(){
+        const translate = 'translate(0,' + this.scrollTop + 'px)';
+        this.querySelector('thead').style.transform = translate;
+    });
+   }, 1000);
   }
 
 }
