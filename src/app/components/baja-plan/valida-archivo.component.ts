bdb8bc2d531ad7122f704acf2b9dbdc39d5a8221
omar_barra <omar_barra_v@hotmail.com>
2018-02-14 11:27:13 -0600

    Estructura telcel

--- src/app/components/baja-plan/valida-archivo.component.ts
+++ src/app/components/baja-plan/valida-archivo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { routerTransition } from '../../router.animations';
+import { routerTransition } from '../shared/router.animations';
 
 @Component({
   templateUrl: './valida-archivo.component.html',
